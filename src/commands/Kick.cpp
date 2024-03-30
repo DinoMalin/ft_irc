47af9fdad3a981a1e5f4d9dd95e169e9a3b3e773
DinoMalin <jcario@student.42lehavre.fr>
2024-03-30 17:08:52 +0100

    ~ | Fixed Kick and made it cleaner

--- src/commands/Kick.cpp
+++ src/commands/Kick.cpp
@@ -1,8 +1,6 @@
 #include "Server.hpp"
 
 void Server::handleKICK(Client &client, Message message) {
-	std::string res;
-
 	if (message.parameters.size() < 2) {
 		sendError(461, client, message, "");
 		return ;
@@ -12,7 +10,7 @@ void Server::handleKICK(Client &client, Message message) {
 		return ;
 	}
 
-	Channel channel = getChannel(message.parameters[0]);
+	Channel &channel = getChannel(message.parameters[0]);
 	if (!channel.isInChannel(client.getNickname()) || !clientExist(message.parameters[1])
 		|| !channel.isInChannel(getClient(message.parameters[1]).getNickname())) {
 		sendError(442, client, message, message.parameters[0]);
@@ -23,16 +21,11 @@ void Server::handleKICK(Client &client, Message message) {
 		return ;
 	}
 
-	getChannel(message.parameters[0]).removeClient(getClient(message.parameters[1]).getNickname());
-	getChannel(message.parameters[0]).removeRegistered(getClient(message.parameters[1]).getNickname());
-	
-	res = ":" + std::string(ADDRESS) + " KICK " + message.parameters[0] + " " + message.parameters[1] + CRLF;
-	std::string kick = ":" + client.getNickname() + "!" + client.getUsername() + "@" + client.getHostname() + " KICK "
-		+ message.parameters[0] + " " + message.parameters[1] + CRLF;
-	if (message.parameters.size() == 3) {
-		res += " :" + message.remainder;
-		kick += " :" + message.remainder;
-	}
-	send(client.getSocket(), res.c_str(), res.length(), 0);
-	send(getClient(message.parameters[1]).getSocket(), kick.c_str(), kick.length(), 0);
+	Client &ClientTarget = getClient(message.parameters[1]);
+
+	std::string res = ":" + client.getSource() + " KICK " + channel.getName() + " " + ClientTarget.getNickname() + " :" + message.remainder + CRLF;
+	channel.sendChannel(res, client, false);
+
+	channel.removeClient(ClientTarget.getNickname());
+	channel.removeRegistered(ClientTarget.getNickname());
 }
