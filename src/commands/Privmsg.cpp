dca46a38b7fd264bb72fba7db4b4baa2609958af
DinoMalin <jcario@student.42lehavre.fr>
2024-04-01 00:22:01 +0200

    + | PRIVMSG now handle correctly multiples targets

--- src/commands/Privmsg.cpp
+++ src/commands/Privmsg.cpp
@@ -12,31 +12,28 @@ void Server::handlePRIVMSG(Client &client, Message message) {
 		return ;
 	}
 
-	for (size_t i = 0; i < message.parameters.size() - 1; i++) {
-		bool keepGoing = false;
-		for (size_t j = 0; j < _channels.size(); j++) {
-			if (_channels[j]->getName() == message.parameters[i])
-				keepGoing = true;
+	std::vector<std::string> params = split(message.parameters[0], ',');
+
+	for (size_t i = 0; i < params.size(); i++) {
+		if (!clientExist(params[i]) && !channelExist(params[i])) {
+			sendError(401, client, message, params[i]);
+			return ;
 		}
-		for (int j = 0; j < _numClients; j++) {
-			if (_clients[j].getNickname() == message.parameters[i])
-				keepGoing = true;
-		}
-		if (!keepGoing) {
-			sendError(401, client, message, message.parameters[i]);
+		if (channelExist(params[i]) && !getChannel(params[i]).isInChannel(client.getNickname())) {
+			sendError(442, client, message, params[i]);
 			return ;
 		}
 	}
 
-	for (size_t i = 0; i < message.parameters.size(); i++) {
-		if (channelExist(message.parameters[i])) {
-			Channel channel = getChannel(message.parameters[i]);
+	for (size_t i = 0; i < params.size(); i++) {
+		if (channelExist(params[i])) {
+			Channel channel = getChannel(params[i]);
 			std::string res = ":" + client.getSource() + " PRIVMSG " + channel.getName() + " :" + message.remainder + CRLF;
-            channel.sendChannel(res, client, true);
+			channel.sendChannel(res, client, true);
 			continue;
 		}
-		if (clientExist(message.parameters[i])) {
-			Client clientTarget = getClient(message.parameters[i]);
+		if (clientExist(params[i])) {
+			Client clientTarget = getClient(params[i]);
 			std::string res = ":" + client.getSource() + " PRIVMSG " + clientTarget.getNickname() + " :" + message.remainder + CRLF;
 			send(clientTarget.getSocket(), res.c_str(), res.length(), 0);
 			continue;
