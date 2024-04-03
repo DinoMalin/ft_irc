ea79e608e66f49025c2df8778d6c53520cf6bf6f
DinoMalin <jcario@student.42lehavre.fr>
2024-04-03 21:55:35 +0200

    ~ | Clean

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -107,10 +107,10 @@ bool Channel::isOperator(std::string nickname) {
 }
 
 void Channel::sendChannel(std::string message, Client author, bool skipAuthor) const {
-    for (size_t i = 0; i < _clients.size(); i++)
-        if (!(skipAuthor && author.getNickname() == _clients[i]->getNickname())) {
+    for (size_t i = 0; i < _clients.size(); i++) {
+        if (!(skipAuthor && author.getNickname() == _clients[i]->getNickname()))
             send(_clients[i]->getSocket(), message.c_str(), message.length(), 0);
-        }
+    }
 }
 
 bool Channel::getCanUseTopic() const {
