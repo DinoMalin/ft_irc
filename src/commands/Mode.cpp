1fad409e07ee0b64a49a540a9d373447868f0d10
DinoMalin <jcario@student.42lehavre.fr>
2024-04-05 19:18:18 +0200

    ~ | Handle multiple mode operation

--- src/commands/Mode.cpp
+++ src/commands/Mode.cpp
@@ -17,12 +17,6 @@ void Server::handleMODE(Client &client, Message message) {
 		return ;
 	}
 
-	for (size_t i = 1; i < message.parameters[1].length(); i++) {
-		if (std::string("itkol").find(message.parameters[1][i]) == std::string::npos) {
-			sendError(472, client, message, std::string() + message.parameters[1][i]);
-			return ;
-		}
-	}
 
 	Channel &channel = getChannel(message.parameters[0]);
 	if (!channel.isInChannel(client.getNickname())) {
@@ -34,59 +28,66 @@ void Server::handleMODE(Client &client, Message message) {
 		return ;
 	}
 
-	char operation = message.parameters[1][0];
-	int flag[127] = {};
-
-	for (size_t  i = 1; i < message.parameters[1].length(); i++) {
-		flag[int(message.parameters[1][i])] = i;
+	int nbParamsNecessary = 0;
+	for (size_t i = 0; i < message.parameters[1].length(); i++) {
+		if (std::string("kol").find(message.parameters[1][i]) != std::string::npos)
+			nbParamsNecessary++;
+		if (std::string("itkol+-").find(message.parameters[1][i]) == std::string::npos) {
+			sendError(472, client, message, std::string("") + message.parameters[1][i]);
+			return ;
+		}
+		if (message.parameters[1][i] == 'o' && !clientExist(message.parameters[1 + nbParamsNecessary])) {
+			sendError(401, client, message, message.parameters[1 + nbParamsNecessary]);
+			return;
+		}
 	}
 
-	if (operation != '+' && operation != '-') {
-		sendError(472, client, message, std::string() + message.parameters[1][1]);
+	if (size_t(nbParamsNecessary + 2) < message.parameters.size()) {
+		sendError(461, client, message, "");
 		return ;
 	}
 
-	size_t nbParamsNecessary = 2;
-	if (flag[int('k')]  && operation == '+')
-		nbParamsNecessary++;
-	if (flag[int('o')])
-		nbParamsNecessary++;
-	if (flag[int('l')] && operation == '+')
-		nbParamsNecessary++;
-	if (message.parameters.size() < nbParamsNecessary) {
-		sendError(461, client, message, "");
-		return ;	
-	}
-
-	if (flag[int('i')]) {
-		if (operation == '+')
-			channel.setInviteOnly(true);
-		else
-			channel.setInviteOnly(false);
-	}
-	if (flag[int('t')]) {
-		if (operation == '+')
-			channel.setCanUseTopic(true);
-		else
-			channel.setCanUseTopic(false);
-	}
-	if (flag[int('k')]) {
-		if (operation == '+')
-			channel.setPassword(message.parameters[1 + flag[int('k')]]);
-		else
-			channel.setPassword("");
-	}
-	if (flag[int('o')]) {
-		if (operation == '+')
-			channel.addOperator(getClient(message.parameters[1 + flag[int('o')]]).getNickname());
-		else
-			channel.removeOperator(getClient(message.parameters[1 + flag[int('o')]]).getNickname());
-	}
-	if (flag[int('l')]) {  
-		if (operation == '+')
-			channel.setLimitUsers(std::atoi(message.parameters[1 + flag[int('l')]].c_str()));
-		else
-			channel.setLimited(false);
+	char operation = 0;
+	int argIndex = 2;
+	for (size_t i = 0; i < message.parameters[1].length(); i++) {
+		switch(message.parameters[1][i]) {
+			case '+':
+				operation = '+';
+				break;
+			case '-':
+				operation = '-';
+				break;
+			case 'i':
+				if (operation == '+')
+					channel.setInviteOnly(true);
+				else
+					channel.setInviteOnly(false);
+				break;
+			case 't':
+				if (operation == '+')
+					channel.setCanUseTopic(true);
+				else
+					channel.setCanUseTopic(false);
+				break;
+			case 'k':
+				if (operation == '+')
+					channel.setPassword(message.parameters[argIndex++]);
+				else
+					channel.setPassword("");
+				break;
+			case 'o':
+				if (operation == '+')
+					channel.addOperator(getClient(message.parameters[argIndex++]).getNickname());
+				else
+					channel.removeOperator(getClient(message.parameters[argIndex++]).getNickname());
+				break;
+			case 'l':
+				if (operation == '+')
+					channel.setLimitUsers(std::atoi(message.parameters[argIndex++].c_str()));
+				else
+					channel.setLimited(false);
+				break;
+		}
 	}
 
 	std::string res = ":" + client.getSource() + " MODE " + channel.getName() + " " + message.parameters[1] + CRLF;
