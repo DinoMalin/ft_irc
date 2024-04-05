152d15fa7d5788c21c62b6ac87ae636c3e9ddd21
DinoMalin <jcario@student.42lehavre.fr>
2024-04-05 13:19:03 +0200

    ~ | Handle multiple keys

--- src/commands/Join.cpp
+++ src/commands/Join.cpp
@@ -7,6 +7,14 @@ void Server::handleJOIN(Client &client, Message message) {
 	}
 
 	std::vector<std::string>channelNames = split(message.parameters[0], ',');
+	std::vector<std::string>keys;
+	if (message.parameters.size() > 1) {
+		keys = split(message.parameters[1], ',');
+		if (keys.size() != channelNames.size()) {
+			sendError(461, client, message, "");
+			return ;
+		}
+	}
 
 	for (size_t i = 0; i < channelNames.size(); i++) {
 		if (channelNames[i][0] != '#')
@@ -27,21 +35,25 @@ void Server::handleJOIN(Client &client, Message message) {
 		}
 		else {
 			Channel &channel = getChannel(channelNames[i]);
-			if (channel.getPassword().length() && message.parameters.size() < 2 && !channel.isRegistered(client.getNickname())) {
+			std::string key;
+			if (keys.size())
+				key = keys[i];
+			
+			if (channel.getPassword().length() && !keys.size() && !channel.isRegistered(client.getNickname())) {
 				sendError(475, client, message, channelNames[i]);
-				return ;
+				continue ;	
 			}
-			if (channel.getPassword().length() && channel.getPassword() != message.parameters[1] && !channel.isRegistered(client.getNickname())) {
+			if (channel.getPassword().length() && channel.getPassword() != key && !channel.isRegistered(client.getNickname())) {
 				sendError(475, client, message, channelNames[i]);
-				return ;
+				continue ;
 			}
 			if (channel.isInviteOnly() && !channel.isRegistered(client.getNickname())) {
 				sendError(473, client, message, channelNames[i]);
-				return ;
+				continue ;
 			}
 			if (channel.isLimited() && channel.getLimitUsers() <= channel.getNbClients()) {
 				sendError(471, client, message, channelNames[i]);
-				return ;
+				continue ;
 			}
 			channel.addClient(client.getNickname());
 			channel.addRegistered(client.getNickname());
